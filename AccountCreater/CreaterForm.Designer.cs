74bc94c21c1268f1b49f775b6fae1c7f7a75eba3
kugool <kugool@172.com>
2016-10-14 11:31:59 +0800

    git inti test

---
+++ AccountCreater/CreaterForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace AccountCreater
+{
+    partial class CreaterForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_Cancle = new System.Windows.Forms.Button();
+            this.tbx_Name = new System.Windows.Forms.TextBox();
+            this.tbx_Password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(178, 104);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(75, 23);
+            this.btn_Save.TabIndex = 2;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // btn_Cancle
+            // 
+            this.btn_Cancle.Location = new System.Drawing.Point(259, 104);
+            this.btn_Cancle.Name = "btn_Cancle";
+            this.btn_Cancle.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancle.TabIndex = 3;
+            this.btn_Cancle.Text = "Cancel";
+            this.btn_Cancle.UseVisualStyleBackColor = true;
+            this.btn_Cancle.Click += new System.EventHandler(this.btn_Cancle_Click);
+            // 
+            // tbx_Name
+            // 
+            this.tbx_Name.Location = new System.Drawing.Point(108, 27);
+            this.tbx_Name.Name = "tbx_Name";
+            this.tbx_Name.Size = new System.Drawing.Size(226, 21);
+            this.tbx_Name.TabIndex = 4;
+            // 
+            // tbx_Password
+            // 
+            this.tbx_Password.Location = new System.Drawing.Point(108, 62);
+            this.tbx_Password.Name = "tbx_Password";
+            this.tbx_Password.Size = new System.Drawing.Size(226, 21);
+            this.tbx_Password.TabIndex = 5;
+            // 
+            // CreaterForm
+            // 
+            this.AcceptButton = this.btn_Save;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 140);
+            this.Controls.Add(this.tbx_Password);
+            this.Controls.Add(this.tbx_Name);
+            this.Controls.Add(this.btn_Cancle);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CreaterForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AccountCreate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Cancle;
+        private System.Windows.Forms.TextBox tbx_Name;
+        private System.Windows.Forms.TextBox tbx_Password;
+    }
+}
+
---
+++ AccountCreater/CreaterForm.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountCreater
+{
+    public partial class CreaterForm : Form
+    {
+        public CreaterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Cancle_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(tbx_Name.Text) || String.IsNullOrEmpty(tbx_Password.Text))
+            {
+                MessageBox.Show("There is something left empty!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            if(tbx_Name.Text.Contains(":"))
+            {
+                MessageBox.Show("You have an invalid character \':\' in user name!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (tbx_Password.Text.Length < 8)
+            {
+                MessageBox.Show("password length is too short, at least 8 characters!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            String md5String = EncryptStringToMD5(this.tbx_Name.Text + ":" + this.tbx_Password.Text, this.tbx_Password.Text);
+
+            if(!String.IsNullOrEmpty(md5String))
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.InitialDirectory = "E:\\";
+                sfd.Filter = "文本文件|*.*|XML文件|*.xml|所有文件|*.*";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    String fName = sfd.FileName;
+                    SaveToFile(fName, this.tbx_Name.Text, md5String);
+                    this.Close();
+                }
+            }
+
+
+        }
+
+        private void SaveToFile(String savePath , String userName, String md5Hash)
+        {
+
+            if (File.Exists(savePath))
+            {
+                UpdateHashFile(savePath, userName, md5Hash);
+            }
+            else
+            {
+                FileStream fs = new FileStream(savePath, FileMode.CreateNew);
+                StreamWriter sw = new StreamWriter(fs);
+                
+                sw.WriteLine(String.Format("{0}:{1}", userName, md5Hash));
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+        }
+
+
+        private void UpdateHashFile(String filePath, String userName, String md5Hash)
+        {
+            Dictionary<String, String> nameHashPairs = GetNameHashPairsFromFile(filePath);
+
+            nameHashPairs[userName] = md5Hash;
+
+            WriteDataToFile(filePath, nameHashPairs);
+        }
+
+        private void WriteDataToFile(String filePath, Dictionary<String, String> nameHashPairs)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+
+            foreach(String key in nameHashPairs.Keys)
+            {
+                sw.WriteLine(String.Format("{0}:{1}", key, nameHashPairs[key]));
+            }
+
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+        private Dictionary<String, String> GetNameHashPairsFromFile(String filePath)
+        {
+            Dictionary<String, String> userHashPair = new Dictionary<string, string>();
+
+            try 
+            {
+                using (StreamReader sr = new StreamReader(filePath)) 
+                {
+                    String line;
+                    while ((line = sr.ReadLine()) != null && (line.Trim() != "")) 
+                    {
+                        String[] userWithHash = line.Split(new char[] { ':' });
+                        
+                        userHashPair.Add(userWithHash[0], userWithHash[1]);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                userHashPair.Clear();
+            }
+
+
+            return userHashPair;
+
+        }
+
+        private  String EncryptStringToMD5(String sinputString, String Skey)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(sinputString);
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(Skey.Substring(0, 8));
+            DES.IV = new byte[] { 0x01, 0x02, 0x03, 0x83, 0x23, 0x67, 0x09, 0x12 };
+            ICryptoTransform desEncrypt = DES.CreateEncryptor();
+            byte[] result = desEncrypt.TransformFinalBlock(data, 0, data.Length);
+
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] output = md5.ComputeHash(data);
+            String encryptResult = BitConverter.ToString(output).Replace("-", "");
+
+            return encryptResult;
+        }
+    }
+}
